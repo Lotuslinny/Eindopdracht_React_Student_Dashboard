eaa1d04635f8b4ef01aa614f0541a26167e61965
Lotuslinny <linny@fegg.nl>
2021-03-08 17:06:24 +0100

    Stop

---
+++ src/Components/DashboardOverview.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { VictoryBar } from 'victory';
+
+class DashboardOverview extends React.Component {
+  render() {
+    return (
+      <div>
+        <VictoryBar />
+      </div>
+    )
+  }
+}
+export default DashboardOverview;
+
