9a10eb035bf64098fbe2d8f140ed016dd6cce17a
Lotuslinny <linny@fegg.nl>
2021-03-12 19:05:09 +0100

    Component Dashboard

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Tabletop from "tabletop";
-import { VictoryChart, VictoryTheme, VictoryBar } from "victory";
-
+import DashboardOverview from './DashboardOverview';
+//import { VictoryChart, VictoryTheme, VictoryBar } from "victory";
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      data: []
-    }
-  }
-  componentDidMount() {
-    Tabletop.init({
-      key: '1Pv1p4vwOe8ZKetA7ealHn-Ulbxp0ycJWwSlF2NZylEE',
-      //key: '1Bh5AV7LwiiWOlK6G-kVDX8YiWJNEyLrrYU6WEYnb_lg',
-
-      callback: googleData => {
-        this.setState({
-          data: googleData
-        })
-      },
-      simpleSheet: true
-    })
-  }
   render() {
-    const { data } = this.state
+    //const { data } = this.state
     // https://formidable.com/open-source/victory/docs/victory-bar/
     return (
+
       <div className="App">
+        <DashboardOverview />
         {/* <header className="App-header">
           <h1 className="App-title" >Student Dashboard</h1>
         </header> */}
-        <h1>Helllooooooooooo</h1>
-        <VictoryChart
+
+        {/* <VictoryChart
           theme={VictoryTheme.material} domainPadding={10} >
           <VictoryBar
             style={{ data: { fill: "#c43a31" } }}
@@ -55,7 +37,7 @@ class App extends Component {
               )
             })
           }
-        </div>
+        </div> */}
       </div>
     );
   }
--- src/DashboardOverview.js
+++ src/DashboardOverview.js
@@ -1,18 +1,43 @@
 import React from "react";
-
-import VictoryChart from "./Components/VictoryChart";
-//import ListOfStudents from "./Components/ListOfStudents";
-
+import Tabletop from "tabletop";
+//import { VictoryChart, VictoryTheme, VictoryBar } from "victory";
 
 class DashboardOverview extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            data: []
+        }
+    }
+    componentDidMount() {
+        Tabletop.init({
+            key: '1Pv1p4vwOe8ZKetA7ealHn-Ulbxp0ycJWwSlF2NZylEE',
+            //key: '1Bh5AV7LwiiWOlK6G-kVDX8YiWJNEyLrrYU6WEYnb_lg',
 
-
+            callback: googleData => {
+                this.setState({
+                    data: googleData
+                })
+            },
+            simpleSheet: true
+        })
+    }
     render() {
+        const { data } = this.state
         return (
-            <div>
-                <VictoryChart data={this.props.data} />
-                {/* <ListOfStudents /> */}
-
+            <div className="DashboardOverview" id="details">
+                {data.map(obj => {
+                    return (
+                        <div key={obj.id}>
+                            <p>
+                                {obj.name} -
+                  {obj.assignment} -
+                  {obj.difficultylevel} -
+                  {obj.funlevel}</p>
+                        </div>
+                    )
+                })
+                }
             </div>
         )
     }
